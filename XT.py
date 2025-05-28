7e10a819fe69c4d2330a087ab6fb18fc54b54e19
Justin Scalpello <163336796+jscalpello@users.noreply.github.com>
2025-05-28 21:09:35 +0000

    Update XT.py

--- XT.py
+++ XT.py
@@ -174,7 +174,7 @@ class higher:
         Returns:
             str: Agents relevant memories from the user input from collections 0-5 and the additional collection number if provided
         """
-        formatted_prompt, prompt, tokens = await self.agent_interactions.format_prompt(
+        formatted_prompt, prompt, tokens = await self.agent_interactions.format_prompt( 
             user_input=user_input if user_input else "*",
             top_results=limit_per_collection,
             min_relevance_score=minimum_relevance_score,
