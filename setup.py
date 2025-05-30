50f727577e374d2a8b90e2ae4ef1f1d681a77c0b
Delaney Menhorst <163336796+delmenhorst09@users.noreply.github.com>
2025-05-30 18:00:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-# Read the contents of your README file
+# Read the contents of your README file 
 this_directory = os.path.dirname(os.path.realpath(__file__))
 readme_path = os.path.join(this_directory, "docs", "README.md")
 
