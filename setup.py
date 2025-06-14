35ed2fc93321e058fe4d435580b5bcffcb67d934
Kash Anderson <163336796+thelegendkaan@users.noreply.github.com>
2025-06-14 01:52:51 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 # Read the contents of your README file 
 this_directory = os.path.dirname(os.path.realpath(__file__))
-readme_path = os.path.join(this_directory, "docs", "README.md")
+readme_path = os.path.join(this_directory, "docs", "README.md") 
  
 # Read the contents of the README file
 with open(readme_path, encoding="utf-8") as f:
